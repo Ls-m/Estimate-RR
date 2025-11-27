c9b0ec24e20587473103101733bf912cb60256eb
ls-m <elham.fr80@gmail.com>
2025-11-27 22:26:16 +0330

    timeonly_v9

--- src/model.py
+++ src/model.py
@@ -83,11 +83,11 @@ class CNNLinearModel(nn.Module):
           hidden_size=2048, output_size=512, dropout=0):
         super(CNNLinearModel, self).__init__()
         self.model = nn.Sequential(
-            nn.Conv1d(in_channels=1, out_channels=2, kernel_size=11, padding=1, stride=3),
+            nn.Conv1d(in_channels=1, out_channels=8, kernel_size=11, padding=1, stride=3),
             nn.ReLU(),
-            nn.Conv1d(in_channels=2, out_channels=2, kernel_size=7, padding=1, stride=2),
+            nn.Conv1d(in_channels=8, out_channels=4, kernel_size=7, padding=1, stride=3),
             nn.ReLU(),
-            nn.Conv1d(in_channels=2, out_channels=2, kernel_size=5, padding=1, stride=2),
+            nn.Conv1d(in_channels=4, out_channels=2, kernel_size=5, padding=1, stride=2),
             nn.ReLU(),
             nn.Conv1d(in_channels=2, out_channels=1, kernel_size=3, padding=1, stride=1),
             nn.ReLU(),
