e7b36a26f1856624bdc3d3e719bd2f79126eb9b0
ls-m <elham.fr80@gmail.com>
2025-12-04 18:25:31 +0330

    freqonly_v106 freq_jitter

--- src/augmenter.py
+++ src/augmenter.py
@@ -61,7 +61,7 @@ class ScalogramAugmentor:
         #     'rotation'
         # ])
         
-        aug_type = 'intensity'
+        aug_type = 'freq_jitter'
 
         if aug_type == 'intensity':
             return self._intensity_augment(scalogram)
