6ce4e562442d44a3a1fdc77d02fd6a36eead908c
ls-m <elham.fr80@gmail.com>
2025-12-22 17:19:55 +0330

    freqonly_v155 train with both, test on bidmc with ssl

--- src/main.py
+++ src/main.py
@@ -3248,7 +3248,7 @@ def main(cfg: DictConfig):
 
 
     # cv_splits = create_balanced_folds(processed_data, n_splits=5)
-    cv_splits = create_folds(processed_data_capnobase, n_splits=5)
+    cv_splits = create_folds(processed_data, n_splits=5)
     logger.info(f"Created folds: {cv_splits}")
 
 
@@ -3258,7 +3258,7 @@ def main(cfg: DictConfig):
         all_test_subjects.update(fold["test_subjects"])
 
     # Collect all subjects in the dataset
-    all_subjects = set(processed_data_capnobase.keys())
+    all_subjects = set(processed_data.keys())
 
     # Check coverage
     missing_subjects = all_subjects - all_test_subjects
@@ -3269,7 +3269,7 @@ def main(cfg: DictConfig):
     print(f"🧩 Missing subjects in test folds: {missing_subjects if missing_subjects else 'None'}")
     print(f"⚠️ Unexpected subjects: {extra_subjects if extra_subjects else 'None'}")
     # processed_data = None
-    all_fold_results = train(cfg, cv_splits, processed_data_capnobase, processed_capnobase_ssl, processed_data)
+    all_fold_results = train(cfg, cv_splits, processed_data, processed_capnobase_ssl, processed_data_capnobase)
     
     for fold_result in all_fold_results:
         logger.info(f"Fold {fold_result['fold_id']} test results: {fold_result['test_results']}")
